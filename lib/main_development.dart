548f9d24ef4f177093233ea5c376bc45328e5a7e
Vi Hoang <viviethoang9@gmail.com>
2022-08-13 00:58:28 +0700

    Test splash screen

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,9 @@
+import 'package:go_router/go_router.dart';
+
 import 'app/app.dart';
 import 'bootstrap.dart';
 
 void main() {
+  GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
   bootstrap(() => const App());
 }
