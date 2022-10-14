d67c5dadb85492eed88a58be73187ff48820f02f
Vi Hoang <viviethoang9@gmail.com>
2022-10-14 23:39:23 +0700

    Lazy loading app

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,10 +1,11 @@
 // ignore: depend_on_referenced_packages
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-import 'app/app.dart';
+import 'app/app.dart' deferred as app;
 import 'bootstrap.dart';
 
-void main() {
+void main() async {
   usePathUrlStrategy();
-  bootstrap(() => const App());
+  await app.loadLibrary();
+  await bootstrap(app.App.new);
 }
