5410affeb0a518cc81723150ec26d656d977483b
Vi Hoang <vihoang@Vis-Mac-mini.local>
2022-10-14 09:10:59 +0700

    Add usePathUrlStrategy

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,10 @@
+// ignore: depend_on_referenced_packages
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
 import 'app/app.dart';
 import 'bootstrap.dart';
 
 void main() {
+  usePathUrlStrategy();
   bootstrap(() => const App());
 }
